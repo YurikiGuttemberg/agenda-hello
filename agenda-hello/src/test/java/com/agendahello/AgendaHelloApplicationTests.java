bea10293468fda56b9968c95868f32d6c3376542
Yuriki Guttemberg <yuriki.gutemberg@gmail.com>
2022-03-16 14:34:26 -0300

    first commit

---
+++ agenda-hello/src/test/java/com/agendahello/AgendaHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendahello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
